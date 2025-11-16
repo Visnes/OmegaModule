0ea21944517f22f7dd434a5d241ae796ec68806a
Kim Visnes <15141314+Visnes@users.noreply.github.com>
2025-11-16 22:43:39 +0100

    New version 1.1.2

--- prototypes/modules.lua
+++ prototypes/modules.lua
@@ -3,17 +3,17 @@ local pollution_enabled = settings.startup["omega-module-pollution"].value
 local omega_module_prototypes = {
   {
     name = "omega-module",
-    effects = { speed = 0.20, consumption = -0.30, productivity = 0.04, quality = 0.1, pollution = pollution_enabled and 0.01 or 0 },
+    effects = { speed = 0.20, consumption = -0.30, productivity = 0.04, quality = 0.1, pollution = pollution_enabled and 0.1 or 0 },
     tier = 1
   },
   {
     name = "omega-module-2",
-    effects = { speed = 0.30, consumption = -0.40, productivity = 0.06, quality = 0.2, pollution = pollution_enabled and 0.025 or 0 },
+    effects = { speed = 0.30, consumption = -0.40, productivity = 0.06, quality = 0.2, pollution = pollution_enabled and 0.25 or 0 },
     tier = 2
   },
   {
     name = "omega-module-3",
-    effects = { speed = 0.50, consumption = -0.50, productivity = 0.10, quality = 0.25, pollution = pollution_enabled and 0.05 or 0 },
+    effects = { speed = 0.50, consumption = -0.50, productivity = 0.10, quality = 0.25, pollution = pollution_enabled and 0.5 or 0 },
     tier = 3
   }
 }
